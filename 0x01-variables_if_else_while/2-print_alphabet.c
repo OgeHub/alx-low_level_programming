63311b95131aa1ee67e5702b043ea3ce91ac5ee2
Success_Ogechi_Ezeh <successogechiezeh@gmail.com>
2023-04-21 08:02:04 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints lowercase alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar("\n");
+	Return 0;
+}
