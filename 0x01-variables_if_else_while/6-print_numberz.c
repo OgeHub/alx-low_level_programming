58bb72ba9e7f99317b232e05872992ace7c0b5b9
Success_Ogechi_Ezeh <successogechiezeh@gmail.com>
2023-04-21 14:51:12 -0700

    numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,14 +2,14 @@
 
 /**
  * main - Entry point
- * print numbers
+ * print number
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num < 10; num++)
 	{
 		putchar('0' + num);
 	}
