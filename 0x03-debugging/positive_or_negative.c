319ba6dad8cf72d20ad0d2cf51340427fc9cf5f4
Success_Ogechi_Ezeh <successogechiezeh@gmail.com>
2023-04-26 22:49:55 -0700

    num

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,25 +3,21 @@
 #include <stdio.h>
 
 /**
- * positive_or_negetive - Entry Ponit
- * @i: integer
- * Function checks integer
- * Return: void
+ *positive_or_negative - Entry point
+ *Print random numbers
+ *Return: Always 0 (Success)
  */
-void positive_or_negative(int i)
+int positive_or_negative(int n)
 {
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-	if (i < 0)
+	if (n > 0)
 	{
-		printf("%d is negative\n", i);
-	}
-	else if (i == 0)
+		printf("%d is positive\n", n);
+	} else if (n == 0)
 	{
-		printf("%d is zero\n", i);
-	}
-	else if (i > 0)
+		printf("%d is zero\n", n);
+	} else if (n < 0)
 	{
-		printf("%d is positive\n", i);
+		printf("%d is negative\n", n);
 	}
+	return (0);
 }
