c1bbff1f758585848eed4a3bcd04830cf592b1b1
Success_Ogechi_Ezeh <successogechiezeh@gmail.com>
2023-05-09 08:11:49 -0700

    password

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_string - funtion name
+ * @s: input 1
+ * @to: input 2
+ * Set string
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
