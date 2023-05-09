b6a64a43423eb6733a598928d512a6e6a446127e
Success_Ogechi_Ezeh <successogechiezeh@gmail.com>
2023-05-08 21:35:59 -0700

    string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strspn - entry point
+ * @s: input
+ * @accept: input
+ *
+ * Return: unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+}
