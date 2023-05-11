566e7ea27ccd9369e0bae54258f5f4d69e7a9498
Success_Ogechi_Ezeh <successogechiezeh@gmail.com>
2023-05-11 10:35:48 -0700

    strings

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_palindrome - function name
+ * @s: input
+ * Is  palindrome
+ * Return: interger
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
