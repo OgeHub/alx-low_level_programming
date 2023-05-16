0233a0ee3ff452b5f33ee6579993ec76ad0820a9
Success_Ogechi_Ezeh <successogechiezeh@gmail.com>
2023-05-16 06:03:11 -0700

    string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include "main.h"
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 
 /**
  * _strdup -funtion name
@@ -10,21 +14,51 @@
  */
 char *_strdup(char *str)
 {
-	int strlength = 0;
-
-	while (*str != '\0')
-	{
-		strlength++;
-		str++;
-	}
-	str = malloc(sizeof(char) * (strlength + 1));
+	int strlength = _strlen(str);
+	char *copy = (char*)malloc(sizeof(char) * strlength);
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	if (!str)
-	{
-		return (NULL);
-	}
+	_strcpy(copy, str);
 	return (str);
+}
+
+/**
+ * _strlen - length of string
+ * @s: character
+ * Function that finds length of string
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
+/**
+ * *_strcpy - copy string
+ * @dest: input character
+ * @src: input character
+ * Fuction that copy string
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
 }
