2f3077036bafb4ffa19db9e069bc41e688f7af2a
Success_Ogechi_Ezeh <successogechiezeh@gmail.com>
2023-05-23 08:01:05 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog details
+ * @name: item
+ * @age: item
+ * @owner: item
+ *
+ * Description: details pf a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
