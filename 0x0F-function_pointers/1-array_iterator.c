b2a1b7b2a43d2631a8a3259f2648c9ab17d7292f
Success Ogechi Ezeh <successogechiezeh@gmail.com>
2023-05-24 16:29:19 +0100

    array interation

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function name
+ * @array: an array
+ * @size: size of the array
+ * @action: function pointer
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t n;
+
+	for (n = 0; n < size; n++)
+	{
+		(*action)(array[n]);
+	}
+}
