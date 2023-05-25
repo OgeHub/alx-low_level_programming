361369f2e85b50b88645348555b1c87f64e6fde3
Success Ogechi Ezeh <successogechiezeh@gmail.com>
2023-05-25 19:59:46 +0100

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,6 +12,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int n;
 
+	if (array == NULL || size <= 0 || cmp == NULL)
+	{
+		return (-1);
+	}
+
 	for (n = 0; n < size; n++)
 	{
 		if ((*cmp)(array[n]))
