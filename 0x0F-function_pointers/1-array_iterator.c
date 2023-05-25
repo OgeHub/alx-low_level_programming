ef15a585c31fc8694b8cbfc603da1645a048306a
Success Ogechi Ezeh <successogechiezeh@gmail.com>
2023-05-25 19:52:31 +0100

    array iteration

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t n;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
 	for (n = 0; n < size; n++)
 	{
 		(*action)(array[n]);
