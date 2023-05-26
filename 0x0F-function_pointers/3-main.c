caa5a3d149d6119be5a7f2110ba9e0018c99233c
Success Ogechi Ezeh <successogechiezeh@gmail.com>
2023-05-26 12:10:01 +0100

    operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,33 @@
  * @argv: array of pointers
  * Return: 0 Success
  */
-int main(int)
+int main(int__attribute__((__unused__))argc, char *argv[])
 {
+	int a, b;
+	char *op;
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	op = argv[2];
+	b = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	
+	printf("%d\n", get_op_func(op)(a, b);
+	return (0);
 }
