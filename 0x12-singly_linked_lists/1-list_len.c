bfadeadc3652620d6b3d897714369e6f2637e35b
Success Ogechi Ezeh <successogechiezeh@gmail.com>
2023-06-01 09:11:46 -0700

    length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list -function name
+ * @h: pointer to the head
+ *
+ * Return: length of list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t length_of_list = 0;
+
+	while( h != NULL)
+	{
+		length_of_list++;
+		h = h->next;
+	}
+	
+	return (length_of_list);
+}
