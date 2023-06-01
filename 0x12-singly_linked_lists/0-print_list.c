b94a09179d20dfe123054065d3b98c5db5f1d375
Success Ogechi Ezeh <successogechiezeh@gmail.com>
2023-06-01 15:04:45 +0100

    prints number of nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - function name
+ * @h: pointer to the header
+ * Prints all elements in a lists
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num_of_nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("%s\n", h->str);
+		else
+			printf("[0] (nil)\n)");
+
+		h = h->next;
+		num_of_nodes++;
+	}
+
+	return (num_of_nodes);
+}
