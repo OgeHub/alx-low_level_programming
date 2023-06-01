6195f520dc63ed7e0cdc5daac77d504ce52fcb0f
Success Ogechi Ezeh <successogechiezeh@gmail.com>
2023-06-01 20:57:16 +0100

    64-bit program

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,9 @@
-section .data
-    format db "Hello, Holberton", 10 ; 10 represents the newline character (\n)
-
-section .text
-    global main
-    extern printf
-
+	global   main
+	extern   printf
 main:
-    ; Load the format string address into the first argument register (rdi)
-    mov rdi, format
-
-    ; Call the printf function
-    call printf
-
-    ; Exit the program
-    mov eax, 0
-    ret
+	mov   edi, format
+	xor   eax, eax
+	call  printf
+	mov   eax, 0
+	ret
+format: db `Hello, Holberton\n`,0
