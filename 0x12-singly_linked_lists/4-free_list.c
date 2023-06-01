8b6600752bd9b23b6b84375b0d3a8a85fef814d2
Success Ogechi Ezeh <successogechiezeh@gmail.com>
2023-06-01 19:33:53 +0100

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - function name
+ * @head: pointer to head
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
