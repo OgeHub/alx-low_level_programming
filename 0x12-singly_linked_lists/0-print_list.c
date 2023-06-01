0033ea9aee523a70c686947dca2752fdca5437ef
Success Ogechi Ezeh <successogechiezeh@gmail.com>
2023-06-01 08:13:24 -0700

    print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,12 @@ size_t print_list(const list_t *h)
 {
 	size_t num_of_nodes = 0;
 
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-		return (0);
-	}
-
 	while (h)
 	{
-		if (h->str != NULL)
+		if (h->str)
 			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 
 		h = h->next;
 		num_of_nodes++;
