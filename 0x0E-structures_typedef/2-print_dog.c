ca73a438393a1fc513fc4dac00be0e3b8805dbcc
Success Ogechi Ezeh <successogechiezeh@gmail.com>
2023-06-02 14:47:39 +0100

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - function name
+ * @d: dog details
+ * Print a dog details
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+
+	if (!(*d).name)
+	{
+		printf("Name: (nil)\n");
+	}
+	else if (!(*d).age)
+	{
+		printf("Age: (nil)\n");
+	}
+	else if (!(*d).owner)
+	{
+		printf("Owner: (nil)\n");
+	}
+
+	printf("Name: %s\n", (*d).name);
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", (*d).owner);
+}
