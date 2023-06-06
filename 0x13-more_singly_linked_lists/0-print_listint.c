d57289c7d093aa3d20b131d11868a2e46bd8a7d3
Success Ogechi Ezeh <successogechiezeh@gmail.com>
2023-06-06 14:03:54 -0700

    print elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - function name
+ * @h: pointer to the header
+ * Prints all elements in a lists
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num_of_nodes = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num_of_nodes++;
+	}
+
+	return (num_of_nodes);
+}
